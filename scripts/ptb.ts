8e5ed7435f286350160c3c24fd72f3a2f79afa46
alex <alex@mystenlabs.com>
2023-10-18 18:55:42 +0300

    clean-up a bit

--- scripts/ptb.ts
+++ scripts/ptb.ts
@@ -3,7 +3,7 @@ import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
 import { SuiClient } from "@mysten/sui.js/client";
 import { fromB64 } from "@mysten/sui.js/utils";
 import * as dotenv from "dotenv";
-import { get } from "http";
+
 dotenv.config();
 
 const pkg = process.env.PACKAGE as string;
@@ -17,7 +17,21 @@ const getDynFields = async () => {
     parentId: shelf,
   });
 
-  console.log(JSON.stringify(response));
+  client.subscribeEvent
+
+  const resp = await client.getObject({
+    id: shelf,
+    options: {
+      showBcs: true,
+      showContent: true,
+      showType: true,
+      showDisplay: true,
+      showPreviousTransaction: true,
+      showOwner: true,
+    },
+  });
+
+  console.log(JSON.stringify(resp));
 };
 
 const getDynField = async () => {
@@ -114,169 +128,10 @@ const main = async () => {
   // const { id: id2, type: type2 } = await mintPotion("LvlUp", keypair, client);
   // const { id: id3, type: type3 } = await mintPotion("STR", keypair, client);
 
-  // const resp1 = await putInShelf(id1, type1, keypair, client);
+  const resp1 = await putInShelf(id1, type1, keypair, client);
   // const resp2 = await putInShelf(id2, type2, keypair, client);
   // const resp3 = await putInShelf(id3, type3, keypair, client);
 
   // console.log(resp1.effects.status.status);
 };
 main();
-// getDynField();
-
-const dyn = {
-  data: [
-    {
-      name: {
-        type: "address",
-        value:
-          "0xeadac26754832d39ecdefac1f13d94bbf5d6fe1bbfb32ccccffc083f59180e6b",
-      },
-      bcsName: "GomseQdnpTj1wUHK3UjrpCKN6i2BTrSjzG8HiRohUZuk",
-      type: "DynamicObject",
-      objectType:
-        "0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::Potion<0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::STR>",
-      objectId:
-        "0xeadac26754832d39ecdefac1f13d94bbf5d6fe1bbfb32ccccffc083f59180e6b",
-      version: 10820584,
-      digest: "EFWNH5Tt1MVaLaZJBhshmxg63AdhmoQX8ZJ2Fqz2MdkU",
-    },
-    {
-      name: {
-        type: "address",
-        value:
-          "0xd01b8b2679c000a73c0e2d34927ab2f0c79768eb0b279f899ce3e923aa9deb06",
-      },
-      bcsName: "F1NAKuzC4oCL6jyhjKbZm3tpLTmdisEA9QYxPZRE9GuK",
-      type: "DynamicObject",
-      objectType:
-        "0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::Potion<0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::LvlUp>",
-      objectId:
-        "0xd01b8b2679c000a73c0e2d34927ab2f0c79768eb0b279f899ce3e923aa9deb06",
-      version: 10820583,
-      digest: "CAUyp5VSTufMMg5XgJ3ZuCxFviV2yf8DssybYpwTxf3j",
-    },
-    {
-      name: {
-        type: "address",
-        value:
-          "0xfc3d6aa78ec3e34d2256541ee393e2959df17a77bc58fd6a956bcc497bc194b8",
-      },
-      bcsName: "Hye4JmQP8FL5o7vhrN4CDKtzUHouvUjFU6tGtHXHnCyu",
-      type: "DynamicObject",
-      objectType:
-        "0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::Potion<0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::CHA>",
-      objectId:
-        "0xfc3d6aa78ec3e34d2256541ee393e2959df17a77bc58fd6a956bcc497bc194b8",
-      version: 10820563,
-      digest: "CtMY2fetcLWS4n6EE8ixAYPodkY5DfnQdg3DNf2Bqp7e",
-    },
-    {
-      name: {
-        type: "address",
-        value:
-          "0xe4e54d8f8d36596b891b808722ab7a2266a65bd7ec4d07a7c4efe7d756595553",
-      },
-      bcsName: "GQWkc4EEvceSLHQNTEvyRcJ84XxdShphbHWQHGEnqjAr",
-      type: "DynamicObject",
-      objectType:
-        "0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::Potion<0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::CHA>",
-      objectId:
-        "0xe4e54d8f8d36596b891b808722ab7a2266a65bd7ec4d07a7c4efe7d756595553",
-      version: 10820582,
-      digest: "2k133jMvtRoL5dMz9zbeRDEtAGSjUPm9SY7u7e3ZFVka",
-    },
-    {
-      name: {
-        type: "address",
-        value:
-          "0x1a1a77313c6b7a41f8f7194864f7217eecaa6085bdd261f0604ec9969782040c",
-      },
-      bcsName: "2ku1Nh84bv4XJBcHJSAUR6g7pveu7w1UsjBmzq2RRZAb",
-      type: "DynamicObject",
-      objectType:
-        "0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::Potion<0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::STR>",
-      objectId:
-        "0x1a1a77313c6b7a41f8f7194864f7217eecaa6085bdd261f0604ec9969782040c",
-      version: 10820578,
-      digest: "nuagbRNdfsCsc23CJJSbUaX28Fq4UXwGXkzzZQCyQSc",
-    },
-    {
-      name: {
-        type: "address",
-        value:
-          "0x1ef360b00bf2fc6382f406a207b700c7882d586d7ad4a80f3e1675ab63cfb6f9",
-      },
-      bcsName: "35pUaDkW7uM5wDC7nJ9cVoaAe3cDkwtAnk4sBFW3hBt4",
-      type: "DynamicObject",
-      objectType:
-        "0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::Potion<0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::CHA>",
-      objectId:
-        "0x1ef360b00bf2fc6382f406a207b700c7882d586d7ad4a80f3e1675ab63cfb6f9",
-      version: 10820570,
-      digest: "6Y4mxNji27qpGqT7HxaHLQ5L6KBnCVg99jqDStjDA8QE",
-    },
-    {
-      name: {
-        type: "address",
-        value:
-          "0xffb37173d61aa220728c8358f3ccf69b5e6e0ba15da7ed1e2512b85907bab2ba",
-      },
-      bcsName: "JD9fUcizfqV7KUk2PEfqCNsasKz3GwhbJ1uh3i14yG2u",
-      type: "DynamicObject",
-      objectType:
-        "0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::Potion<0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::LvlUp>",
-      objectId:
-        "0xffb37173d61aa220728c8358f3ccf69b5e6e0ba15da7ed1e2512b85907bab2ba",
-      version: 10820577,
-      digest: "FoH7ZszsDxXVrC8otQVTAp12nHAwfmvGJrukm6wCn6dF",
-    },
-    {
-      name: {
-        type: "address",
-        value:
-          "0x89bfb53ebef3df00cb1943dd68ee0357b91ecd3fc62e62cc321b18d6cb6f7da9",
-      },
-      bcsName: "AGiQKhYoBFBEV9BWGSGTUY1NiasMA1oMxMeJS84psmtC",
-      type: "DynamicObject",
-      objectType:
-        "0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::Potion<0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::CHA>",
-      objectId:
-        "0x89bfb53ebef3df00cb1943dd68ee0357b91ecd3fc62e62cc321b18d6cb6f7da9",
-      version: 10820576,
-      digest: "ChubcJbCDwoTmqXvxandJDwiDn1Z3EbrkMqBxvTDufCY",
-    },
-    {
-      name: {
-        type: "address",
-        value:
-          "0x2c108736016b3bf0bb8ed7e3515c79aa0809ea7d1755e6ba20b8301c70e669d7",
-      },
-      bcsName: "3y1ZGPCZgtg6ZUNPjwrm3E1nq5UsAQktC5mCjCbuGoSW",
-      type: "DynamicObject",
-      objectType:
-        "0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::Potion<0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::STR>",
-      objectId:
-        "0x2c108736016b3bf0bb8ed7e3515c79aa0809ea7d1755e6ba20b8301c70e669d7",
-      version: 10820572,
-      digest: "251X5SHiP25NQv2dZmDD41MV8R72bMmvQ5gseCEijU6C",
-    },
-    {
-      name: {
-        type: "address",
-        value:
-          "0x06e81bf4b58040b51f935512ba25b3d1e5a7b18a12d57043ab8b0e182397ccb5",
-      },
-      bcsName: "Txj32t345Z4BoMBFXFPxn2mgiPw6uQHMWPM5RtkUw2U",
-      type: "DynamicObject",
-      objectType:
-        "0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::Potion<0x7dcef1556fb2033dc6f5fe3f1d43fa6e8cb1a0d740c039d398bff14761ad4209::potions::LvlUp>",
-      objectId:
-        "0x06e81bf4b58040b51f935512ba25b3d1e5a7b18a12d57043ab8b0e182397ccb5",
-      version: 10820571,
-      digest: "FDRsPdH1Kf3m2UN4Cv2KzBWAkcwDG2QFEUqnLUa91DYU",
-    },
-  ],
-  nextCursor:
-    "0xf9d2320f15238c7988f344e6028c81d9f3b2d57c9274bfeb9aa955add3a5fbbc",
-  hasNextPage: false,
-};
